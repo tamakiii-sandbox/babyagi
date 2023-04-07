73dd4ccef6af6a6fc213b0cabf999b6f945e3f84
tamakiii <tamakiii@users.noreply.github.com>
2023-04-07 16:12:30 +0900

    fix docker.mk

--- docker.mk
+++ docker.mk
@@ -1,15 +1,16 @@
-.PHONY: help
+.PHONY: help build bash clean
 
-TAG := tamakiii-sandbox/babyagi
+NAME := tamakiii-sandbox/babyagi
+TAG := latest
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
 build: Dockerfile
-	docker build -t $(TAG) .
+	docker build -t $(NAME):$(TAG) .
 
 bash: build
-	docker run -it --rm $(TAG) $@
+	docker run -it --rm $(NAME):$(TAG) $@
 
 clean:
-	docker image rm $(TAG)
+	docker image rm $(NAME):$(TAG)
