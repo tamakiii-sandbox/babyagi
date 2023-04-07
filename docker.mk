814e685677cfc7c498913c37383722adcae396e1
tamakiii <tamakiii@users.noreply.github.com>
2023-04-07 16:34:54 +0900

    add volume and workdir

--- docker.mk
+++ docker.mk
@@ -10,7 +10,7 @@ build: Dockerfile
 	docker build -t $(NAME):$(TAG) .
 
 bash: build
-	docker run -it --rm $(NAME):$(TAG) $@
+	docker run -it -v $(PWD):/usr/local/lib/$(NAME) -w /usr/local/lib/$(NAME) --rm $(NAME):$(TAG) $@
 
 clean:
 	docker image rm $(NAME):$(TAG)
